554e59d5f95031b070dc73c4ecfea50583c3b0e7
Oliver Atkinson <oliveratkinsoneng@gmail.com>
2023-07-16 18:55:31 -0600

    changed to lib

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+#![feature(trait_alias)]
+
+mod test;
+mod db;
+mod schema;
+mod auth;
+
+pub mod pages;
+
+use std::collections::HashMap;
+
+use crate::auth::keyring::Keyring;
+use auth::authentication::Uuid;
+use rocket::tokio::sync::RwLock;
+
+#[cfg(not(feature = "redis"))]
+pub type ManagedState = RwLock<Keyring<HashMap<Uuid, String>>>;
+
+#[cfg(feature = "redis")]
+pub type ManagedState = RwLock<Keyring<redis::Connection>>;
+
+pub fn get_state() -> ManagedState {
+    #[cfg(feature = "redis")]
+    return RwLock::new(Keyring { ring: Box::new(db::redis_connect().unwrap()) } );
+
+    #[cfg(not(feature = "redis"))]
+    return RwLock::new(Keyring { ring: Box::new(HashMap::<Uuid, String>::new()) } );
+}
